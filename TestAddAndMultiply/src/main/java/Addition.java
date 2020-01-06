cbe6c01cc6769e098429ea52b81b03dcb7ea286c
yuan3675 <yuan3675@gmail.com>
2020-01-06 15:22:46 +0800

    remove code

--- TestAddAndMultiply/src/main/java/Addition.java
+++
@@ -1,6 +0,0 @@
-public class Addition {
-
-    public int add(String n1, String n2){
-        return Integer.parseInt(n1)+Integer.parseInt(n2);
-    }
-}
--- TestAddAndMultiply/src/main/java/Multiplication.java
+++
@@ -1,6 +0,0 @@
-public class Multiplication {
-
-    public int multiply(String n1, String n2){
-        return Integer.parseInt(n1)*Integer.parseInt(n2);
-    }
-}
